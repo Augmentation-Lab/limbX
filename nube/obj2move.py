22d90247c040e2ce54dfd1b5c4a2fd2a22a631ba
Caine <24969357+CaineArdayfio@users.noreply.github.com>
2022-08-04 19:35:10 -0400

    Added nube folder

---
+++ nube/obj2move.py
@@ -0,0 +1,12 @@
+'''
+INPUT: A vector of an intended location (target object location - current location)
+OUTPUT: Movement of servos for the next 100ms
+'''
+
+intPos = [3, 3, 5]
+
+initServoPos = [30, 45, 170]
+
+
+
+
