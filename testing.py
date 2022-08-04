6da72575a1d0293657e7b23e5d63026af4dec39c
Alice Cai <alicecai@MacBook-Pro-4.local>
2022-08-04 18:54:14 -0400

    adding test script

--- testing.py
+++ testing.py
@@ -1,30 +1,21 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
-servoPIN = 12
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+SERVO_PIN = 12
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(0) # Initialization
-try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+pwm=GPIO.PWM(SERVO_PIN, 50)
+pwm.start(0)
+
+def SetAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(SERVO_PIN, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(SERVO_PIN, False)
+	pwm.ChangeDutyCycle(0)
+
+SetAngle(90) 
+pwm.stop()
+GPIO.cleanup()
