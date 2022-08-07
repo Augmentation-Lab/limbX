c660679a4833c9aaaa6b55c84556e4046d87505f
Alice Cai <alicecai@MacBook-Pro-4.local>
2022-08-07 01:09:11 -0400

    COMMON POINT FOR CAINE

---
+++ archived/limb_old/demo.py
@@ -0,0 +1,83 @@
+""""
+DEMO SCRIPT
+demonstrates tentacle functionality
+"""
+import driver
+from time import sleep
+
+def demo(demoMoveSegments=False, demoExploreWorkspace=False, demoGrab=False, demoRelease=False, demoWaveHello=False):
+    if demoMoveSegments: moveSegments()
+    if demoExploreWorkspace: exploreWorkspace()
+    if demoGrab: grab()
+    if demoRelease: release()
+    if demoWaveHello: waveHello()
+    driver.shutdown()
+
+demo(demoMoveSegments=True, demoGrab=True, demoRelease=True)
+
+# SINGLE MOVES
+
+def moveTo(targetRelPos):
+    print("moveTo(target={target})")
+    driver.move(targetRelPos)
+
+def grab():
+    print("grab()")
+    driver.grab()
+
+def release():
+    print("release()")
+    driver.release()
+
+# SEQUENCES
+
+def moveSegments():
+    print("demoSegments()")
+    driver.resetAngles()
+    anglesDict = {
+        "home": {"lr": 0, "ud": 0},
+        "left": {"lr": 90, "ud": 0},
+        "right": {"lr": -90, "ud": 0},
+        "up": {"lr": 0, "ud": 90},
+        "down": {"lr": 0, "ud": -90},
+        "upleft": {"lr": 90, "ud": 90},
+        "upright": {"lr": -90, "ud": 90},
+        "downleft": {"lr": 90, "ud": -90},
+        "downright": {"lr": -90, "ud": -90},
+    }
+    
+    movements = [
+        "home", "left", 
+        "home", "right",
+        "home", "up",
+        "home", "down",
+        "home", "upleft",
+        "home", "upright",
+        "home", "downleft",
+        "home", "downright",
+        "home"
+        ]
+
+    for segment in range(1, len(driver.systemSTATE.numSegments)+1):
+        for movement in movements:
+            driver.moveSegment(segment, anglesDict[movement])
+            sleep(1)
+
+def exploreWorkspace():
+    driver.resetAngles()
+    # implement later: moveTo spherical points within workspace, via producing all combinations of angles given numSegments
+    print("exploreWorkspace()")
+
+def waveHello():
+    # implement later
+    print("waveHello()")
+
+
+# only implementable in integration with UI
+# def moveAndGrab(targetObj):
+#     print(f"moveAndGrab(object={targetObj})")
+#     driver.executeCommands([{"move": targetObj}, {"grab": targetObj}])
+
+# def moveAndRelease(targetObj):
+#     print(f"moveAndRelease(object={targetObj})")
+#     driver.executeCommands([{"move": targetObj}, {"release": targetObj}])
