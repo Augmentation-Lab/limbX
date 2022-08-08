cb0897766f0e70c9e59982eff3c20e3b95de37bc
Limb RPi <cyborgs.harvard@gmail.com>
2022-08-08 11:51:57 -0400

    Fixed testing scripts

---
+++ testing.py
@@ -0,0 +1,39 @@
+from limb import driver
+from limb.utilities import servo
+from time import sleep
+
+driver.initialize()
+
+batchAngles11 = {
+    1: {
+        "lr": 90,
+        "ud": 90
+    }
+}
+batchAngles12 = {
+    1: {
+        "lr": 0,
+        "ud": 0
+    }
+}
+batchAngles21 = {
+    2: {
+        "lr": 90,
+        "ud": 90
+    }
+}
+batchAngles22 = {
+    2: {
+        "lr": 0,
+        "ud": 0
+    }
+}
+
+servo.batchSetAngles(driver.systemSTATE.servoDict, batchAngles11)
+sleep(1)
+servo.batchSetAngles(driver.systemSTATE.servoDict, batchAngles12)
+sleep(1)
+servo.setAllAngles(driver.systemSTATE.servoDict, 10)
+
+
+driver.shutdown()
